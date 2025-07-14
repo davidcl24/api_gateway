11bd7a647f138fbea108976201450281a2ea25d8
davidcl24 <davidcl.flor@gmail.com>
2025-07-14 23:01:40 +0200

    registered proxies in gateway

--- src/gateway.js
+++ src/gateway.js
@@ -2,12 +2,16 @@ import Fastify from 'fastify';
 import jwtPlugin from './plugins/jwt';
 import favouritesProxy from './routes/favourites';
 import historyProxy from './routes/history';
+import usersProxy from './routes/users';
+import contentsProxy from './routes/contents';
 
 const fastify = Fastify({ logger: true });
 
 await fastify.register(jwtPlugin);
 await fastify.register(favouritesProxy);
 await fastify.register(historyProxy);
+await fastify.register(usersProxy);
+await fastify.register(contentsProxy);
 
 
 fastify.listen({ port: 30000 }, err => {
