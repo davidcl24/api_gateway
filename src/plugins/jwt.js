a3423aa9393c5d4b99dc385547d9ef1a9cbafd2e
davidcl24 <davidcl.flor@gmail.com>
2025-07-29 17:12:29 +0200

    the authenticatin didn't work because of the secret

--- src/plugins/jwt.js
+++ src/plugins/jwt.js
@@ -5,25 +5,33 @@ import fp from 'fastify-plugin';
 
 export default fp(
   async function jwtPlugin(fastify, opts) {
+      const secret = process.env.SECRET_KEY || 'dev_key';
+
       fastify.register(fastifyJwt, {
-          secret: String(process.env.SECRET_KEY) || 'dev_key',
+          secret: secret,
+          sign: { algorithm: 'HS256' },
       });
 
       fastify.register(fastifyCookie, {
-          secret: process.env.SECRET_KEY || 'dev_key',
+          secret: secret,
           hook: "preHandler",
       });
 
       fastify.decorate("authenticate", async function (request, reply) {
       try {
-        const token = request.cookies.token;
+        const token = request.cookies.access_token;
+
         if (!token) {
           return reply.status(401).send({ message: "Auth required" });
         }
 
         await request.jwtVerify(token);
       } catch (err) {
-        reply.code(401).send({ error: 'Invalid or absent token' });
+        if (err.name === 'TokenExpiredError')
+          reply.code(401).send({ error: 'Token expired' });
+
+        if (err.name === 'JsonWebTokenError')
+          reply.code(401).send({ error: 'JWT error: ' + err.message  });
       }
     });
   }
