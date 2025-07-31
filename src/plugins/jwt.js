cc44d2c2191b48dfa6e1b42ac6996a10bbd8feb0
davidcl24 <davidcl.flor@gmail.com>
2025-07-31 15:54:17 +0200

    jwt plugin now checks both refresh token and access token

--- src/plugins/jwt.js
+++ src/plugins/jwt.js
@@ -5,6 +5,7 @@ import fp from 'fastify-plugin';
 
 export default fp(
   async function jwtPlugin(fastify, opts) {
+      const usersServiceUrl = process.env.USERS_SERVICE_URL || 'http://localhost:4000/api';
       const secret = process.env.SECRET_KEY || 'dev_key';
 
       fastify.register(fastifyJwt, {
@@ -19,20 +20,42 @@ export default fp(
 
       fastify.decorate("authenticate", async function (request, reply) {
       try {
-        const token = request.cookies.access_token;
-
-        if (!token) {
-          return reply.status(401).send({ message: "Auth required" });
-        }
-
-        await request.jwtVerify(token);
+        const accessToken = request.cookies.access_token;
+        await request.jwtVerify(accessToken);
       } catch (err) {
-        if (err.name === 'TokenExpiredError')
+        const refreshToken = request.cookies.refresh_token;
+        
+        if (!refreshToken) {
+          return reply.status(401).send({ message: "Unauthorized" });
+        }
+        try {
+          await request.jwtVerify(refreshToken);
+
+          const res = await fetch(`${usersServiceUrl}/refresh`, {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json',
+                  'x-user-id': request.user.sub
+              },
+              body: JSON.stringify(request.body),
+          });
+
+        const setCookies = res.headers.getSetCookie();
+        if (setCookies) {
+            for (const cookie of setCookies) {
+                reply.header('set-cookie', cookie);
+            }
+        }
+        const data = await res.json();
+        return reply.send(data)
+        } catch (err) {
+          if (err.name === 'TokenExpiredError')
           reply.code(401).send({ error: 'Token expired' });
 
         if (err.name === 'JsonWebTokenError')
           reply.code(401).send({ error: 'JWT error: ' + err.message  });
       }
+        }
     });
   }
 )
