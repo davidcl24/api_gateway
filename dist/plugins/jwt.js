362809983d9c7c4804923453dd37b044982d0f76
davidcl24 <davidcl.flor@gmail.com>
2025-10-13 23:58:40 +0200

    changed jwt plugin so it recieves tokens from httponly cookies

--- dist/plugins/jwt.js
+++ dist/plugins/jwt.js
@@ -1,6 +1,7 @@
 import fastifyJwt from '@fastify/jwt';
 import fastifyCookie from '@fastify/cookie';
 import fp from 'fastify-plugin';
+import { log } from 'console';
 export default fp(async function jwtPlugin(fastify, opts) {
     const secret = process.env.SECRET_KEY || 'dev_key';
     fastify.register(fastifyJwt, {
@@ -9,17 +10,68 @@ export default fp(async function jwtPlugin(fastify, opts) {
     });
     fastify.register(fastifyCookie, {
         secret: secret,
-        hook: "preHandler",
     });
     fastify.decorate("authenticate", async function (request, reply) {
         try {
-            await request.jwtVerify();
+            log('me cago');
+            var accessToken = request.cookies.access_token;
+            log('access token: ', accessToken);
+            var refreshToken = request.cookies.refresh_token;
+            log('refresh Token: ', refreshToken);
+            if (!accessToken && !refreshToken) {
+                reply.code(401).send({ error: 'No tokens provided' });
+                return;
+            }
+            let decoded;
+            if (accessToken) {
+                decoded = await request.jwtVerify({ jwt: accessToken });
+            }
+            else {
+                throw new Error('Access token missing or invalid');
+            }
+            request.user = decoded;
         }
         catch (err) {
-            if (err.name === 'TokenExpiredError')
-                reply.code(401).send({ error: 'Token expired' });
-            if (err.name === 'JsonWebTokenError')
-                reply.code(401).send({ error: 'JWT error: ' + err.message });
+            if (err.message === 'Access token missing or invalid') {
+                if (!refreshToken) {
+                    reply.code(401).send({ error: 'Refresh token missing' });
+                    return;
+                }
+                try {
+                    const refreshDecoded = await request.jwtVerify({ jwt: refreshToken });
+                    const res = await fetch(`http://localhost:4000/api/refresh`, {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'x-user-id': refreshDecoded.sub
+                        },
+                        body: JSON.stringify(request.body),
+                    });
+                    const setCookies = res.headers.getSetCookie();
+                    if (setCookies) {
+                        for (const cookie of setCookies) {
+                            reply.header('set-cookie', cookie);
+                        }
+                    }
+                    request.user = refreshDecoded;
+                }
+                catch (refreshErr) {
+                    if (refreshErr.name === 'TokenExpiredError') {
+                        reply.code(401).send({ error: 'Refresh token expired' });
+                    }
+                    else if (refreshErr.name === 'JsonWebTokenError') {
+                        reply.code(401).send({ error: 'Refresh token invalid' });
+                    }
+                    else {
+                        reply.code(401).send({ error: 'Authentication error' });
+                    }
+                    return;
+                }
+            }
+            else {
+                reply.code(401).send({ error: 'Authentication error' });
+                return;
+            }
         }
     });
 });
