80ebabfaaf68569b47b4bf02527e9b455a55813d
davidcl24 <davidcl.flor@gmail.com>
2025-10-19 19:16:12 +0200

    added personal endpoints to history and favourites

--- src/routes/favourites.ts
+++ src/routes/favourites.ts
@@ -56,6 +56,55 @@ export default async function favouritesProxy(fastify: FastifyInstance, opts: Fa
         });
     });
 
+
+    fastify.get('/favourites/user/personal', { preHandler: [fastify.authenticate] },  async (request, reply) => {
+        const sub = request.user.sub;
+
+        const res = await fetch(`${favsServiceUrl}/api/favourites/user/${sub}`, {
+            method: 'GET',
+        });
+
+        const favourites = await res.json();
+
+        const movieIds = Array.isArray(favourites)
+            ? favourites
+                .filter(fav => fav.movie_id && fav.movie_id !== 0)
+                .map(fav => fav.movie_id)
+            : [];
+        const showIds = Array.isArray(favourites)
+            ? favourites
+                .filter(fav => fav.show_id && fav.show_id !== 0)
+                .map(fav => fav.show_id)
+            : [];
+
+
+        const [movieRes, showRes] = await Promise.all([
+            fetch(`${contentsServiceUrl}/movies/batch`, {
+                method: 'POST',
+                headers: {
+                    "Content-Type": 'application/json',
+                },
+                body: JSON.stringify(movieIds),
+            }),
+
+            fetch(`${contentsServiceUrl}/shows/batch`, {
+                method: 'POST',
+                headers: {
+                    "Content-Type": 'application/json',
+                },
+                body: JSON.stringify(showIds),
+            }),
+        ]);
+
+        const movieData = await movieRes.json();
+        const showData = await showRes.json();
+
+        return reply.send({
+            movies: movieData,
+            shows: showData,
+        });
+    });
+
     fastify.register(httpProxy, {
         upstream: favsServiceUrl,
         prefix: '/favourites',
--- src/routes/history.ts
+++ src/routes/history.ts
@@ -3,6 +3,18 @@ import type { FastifyInstance, FastifyPluginOptions } from 'fastify';
 
 export default async function historyProxy(fastify: FastifyInstance, opts: FastifyPluginOptions) {
     const historyServiceUrl = process.env.HISTORY_SERVICE_URL || 'http://localhost:7500'
+
+    fastify.get('/history/user/personal', { preHandler: [fastify.authenticate] }, async(request, reply) => {
+        const sub = request.user.sub;
+
+        const res = await fetch(`${historyServiceUrl}/api/history/user/${sub}`, {
+            method: 'GET',
+        });
+
+        const data = await res.json();
+        return reply.send(data);
+    });
+
     fastify.register(httpProxy, {
         upstream: historyServiceUrl,
         prefix: '/history',
