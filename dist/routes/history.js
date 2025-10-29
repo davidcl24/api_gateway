a3739e8d8dd5304389b8b96b571915b9cfd0696f
davidcl24 <davidcl.flor@gmail.com>
2025-10-29 21:33:56 +0100

    new endpoint for history

--- dist/routes/history.js
+++ dist/routes/history.js
@@ -9,6 +9,41 @@ export default async function historyProxy(fastify, opts) {
         const data = await res.json();
         return reply.send(data);
     });
+    fastify.get('/history/user/personal/movie/:id', { preHandler: [fastify.authenticate] }, async (request, reply) => {
+        const sub = request.user.sub;
+        const movieId = request.params.id;
+        const res = await fetch(`${historyServiceUrl}/api/history/user/${sub}/movie/${movieId}`, {
+            method: 'GET',
+        });
+        const data = await res.json();
+        return reply.send(data);
+    });
+    fastify.get('/history/user/personal/episode/:id', { preHandler: [fastify.authenticate] }, async (request, reply) => {
+        const sub = request.user.sub;
+        const showId = request.params.id;
+        const res = await fetch(`${historyServiceUrl}/api/history/user/${sub}/episode/${showId}`, {
+            method: 'GET',
+        });
+        const data = await res.json();
+        return reply.send(data);
+    });
+    fastify.post('/history/new', { preHandler: [fastify.authenticate] }, async (request, reply) => {
+        const sub = request.user.sub;
+        const originalBody = request.body;
+        request.body = {
+            ...originalBody,
+            user_id: sub
+        };
+        const res = await fetch(`${historyServiceUrl}/api/history`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": 'application/json',
+            },
+            body: JSON.stringify(request.body),
+        });
+        const data = await res.json();
+        return reply.send(data);
+    });
     fastify.register(httpProxy, {
         upstream: historyServiceUrl,
         prefix: '/history',
--- src/routes/history.ts
+++ src/routes/history.ts
@@ -2,6 +2,9 @@ import httpProxy from '@fastify/http-proxy';
 import type { FastifyInstance, FastifyPluginOptions } from 'fastify';
 
 export default async function historyProxy(fastify: FastifyInstance, opts: FastifyPluginOptions) {
+      interface Params {
+        id: string,
+    }
     const historyServiceUrl = process.env.HISTORY_SERVICE_URL || 'http://localhost:7500'
 
     fastify.get('/history/user/personal', { preHandler: [fastify.authenticate] }, async(request, reply) => {
@@ -11,6 +14,48 @@ export default async function historyProxy(fastify: FastifyInstance, opts: Fasti
             method: 'GET',
         });
 
+        const data = await res.json();
+        return reply.send(data);
+    });
+
+    fastify.get<{ Params: Params }>('/history/user/personal/movie/:id', { preHandler: [fastify.authenticate] }, async(request, reply) => {
+        const sub = request.user.sub;
+        const movieId = request.params.id;
+
+        const res = await fetch(`${historyServiceUrl}/api/history/user/${sub}/movie/${movieId}`, {
+            method: 'GET',
+        });
+        const data = await res.json();
+        return reply.send(data)
+    });
+
+    fastify.get<{ Params: Params }>('/history/user/personal/episode/:id', { preHandler: [fastify.authenticate] }, async(request, reply) => {
+        const sub = request.user.sub;
+        const showId = request.params.id;
+
+        const res = await fetch(`${historyServiceUrl}/api/history/user/${sub}/episode/${showId}`, {
+            method: 'GET',
+        });
+        const data = await res.json();
+        return reply.send(data)
+    });
+
+    fastify.post('/history/new', { preHandler: [fastify.authenticate] }, async(request, reply) => {
+        const sub = request.user.sub;
+        const originalBody = request.body as any;
+
+        request.body = {
+            ...originalBody,
+            user_id: sub
+        }
+
+        const res = await fetch(`${historyServiceUrl}/api/history`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": 'application/json',
+            },
+            body: JSON.stringify(request.body),
+        });
         const data = await res.json();
         return reply.send(data);
     });
